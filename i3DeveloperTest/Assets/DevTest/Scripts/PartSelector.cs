7b36aec426e8af6784dd2bdd4a25a797fb76eb8a
acanady <alfonso.canadyjr@gmail.com>
2021-10-30 19:28:55 -0500

    added basic script and folder hierarchy, initial scene layout

---
+++ i3DeveloperTest/Assets/DevTest/Scripts/PartSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Parts))]
+public class PartSelector : MonoBehaviour
+{
+    private List<GameObject> selectable_parts;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectable_parts = gameObject.GetComponent<Parts>().selectable_parts;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            print(hit.collider.name);
+            
+        }
+    }
+}
---
+++ i3DeveloperTest/Assets/DevTest/Scripts/Parts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Class containing the selectable parts for an object, parts set in inspector
+public class Parts : MonoBehaviour
+{
+
+    public List<GameObject> selectable_parts;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
