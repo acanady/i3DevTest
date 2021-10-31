c4fe9bc139fcf3f0a9a3aa7d18134aa1c4b132a3
acanady <alfonso.canadyjr@gmail.com>
2021-10-31 02:05:48 -0500

    fixed camera movement issue and implemented new camera system that uses set co-ordinates to determine rotation and position

--- i3DeveloperTest/Assets/DevTest/Scripts/CameraController.cs
+++ i3DeveloperTest/Assets/DevTest/Scripts/CameraController.cs
@@ -6,15 +6,17 @@ public class CameraController : MonoBehaviour
 {
     public Camera camera;
     private GameObject viewed_part;
-    private Vector3 offset;
-    private Vector3 original_pos;
+    private Vector3 position;
+    private Vector3 rotation;
+    private float duration;
+    private bool lerping;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        duration = .5f;
         camera = Camera.main;
-        original_pos = camera.transform.position;
+        
     }
 
     // Update is called once per frame
@@ -26,20 +28,26 @@ public class CameraController : MonoBehaviour
     //Moves camera to specific game obect location and looks at it
     public void MoveToPart(GameObject part)
     {
-        camera.transform.position = original_pos;
-        viewed_part = part;
-
-        //Need a better way of handling objects that require a different zoom level
-        if(part.name == "SkyCarBody")
-        {
-            Vector3 direction = (camera.transform.position - part.transform.position).normalized;
-            camera.transform.position = part.transform.position + direction * 4f;
-        }
+        position = part.GetComponent<LookPosition>().position;
+        rotation = part.GetComponent<LookPosition>().rotation;
+        camera.transform.eulerAngles = rotation;
+        if(!lerping)
+            StartCoroutine(Lerp());
         
-        else
-        {
-            Vector3 direction = (camera.transform.position - part.transform.position).normalized;
-            camera.transform.position = part.transform.position + direction * 3f;
-        }
     }
+
+    IEnumerator Lerp()
+    {
+        lerping = true;
+        float time = 0;
+        while(time < duration)
+        {
+            camera.transform.position = Vector3.Lerp(camera.transform.position,position, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        lerping = false;
+        camera.transform.position = position;
+    }
+
 }
---
+++ i3DeveloperTest/Assets/DevTest/Scripts/LookPosition.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class that stores the position and rotation of the camera for the specific object
+public class LookPosition : MonoBehaviour
+{
+    public Vector3 position;
+    public Vector3 rotation;
+}
